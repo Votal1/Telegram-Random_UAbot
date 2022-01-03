203d90e12e0fbdfb0732fb31950ac6e2b1043128
Votal1 <vitaliypankiv1@gmail.com>
2022-01-03 22:26:39 +0200

    cancel

--- main.py
+++ main.py
@@ -8,72 +8,13 @@ from datetime import date
 from datetime import datetime
 import time
 
+from variables import names, icons, class_name, weapons, defenses, supports, photos, sudoers
+
 r = redis.Redis(host=os.environ.get('REDIS_HOST'), port=int(os.environ.get('REDIS_PORT')),
                 password=os.environ.get('REDIS_PASSWORD'), db=0)
 
 TOKEN = os.environ.get('TOKEN')
 bot = telebot.TeleBot(TOKEN)
-
-names = ['Ваня', 'Ваня', 'Ваня', 'Славік', 'Колян', 'Вован', 'Мага', 'Махмед', 'Олег', 'Стасян',
-         'Петруха', 'Алєксандр', 'Сашка', 'Міхаіл', 'Мішка', 'Юрас', 'Гіві', 'Андрюха', 'Хабіб',
-         'Саньок', 'Льоха', 'Даня', 'Русік', 'Альоша', 'Вітя', 'Владос', 'Женя', 'Ігор', 'Сірьога',
-         'Едік', 'Віталя', 'Ашот', 'Моторола']
-
-icons = ['', '\U0001F919', '\U0001F9F0', '\U0001F52E', '\U0001F5FF',
-         '\U0001fa96', '\U0001F46E', '\U0001F921', '\U0001F4DF', '', '',
-         '\U0001F919\U0001F919', '\U0001F9F0\U0001F9F0', '\U0001F52E\U0001F52E', '\U0001F5FF\U0001F5FF',
-         '\U0001fa96\U0001fa96', '\U0001F46E\U0001F46E', '\U0001F921\U0001F921', '\U0001F4DF\U0001F4DF', '', '',
-
-         '\U0001F919\U0001F919\U0001F919', '\U0001F9F0\U0001F9F0\U0001F9F0', '\U0001F52E\U0001F52E\U0001F52E',
-         '\U0001F5FF\U0001F5FF\U0001F5FF', '\U0001fa96\U0001fa96\U0001fa96', '\U0001F46E\U0001F46E\U0001F46E',
-         '\U0001F921\U0001F921\U0001F921', '\U0001F4DF\U0001F4DF\U0001F4DF']
-
-class_name = ['', 'Хач', 'Роботяга', 'Фокусник', 'Язичник', 'Гарматне м`ясо', 'Мусор', 'Малорос', 'Хакер', '', '',
-              'Борцуха', 'Почесний алкаш', 'Злий геній', 'Скінхед', 'Орк', 'Силовик', 'Кремлебот', 'Кіберзлочинець',
-              '', '', 'Гроза Кавказу', 'П`яний майстер', 'Некромант', 'Білий вождь', 'Герой Донбасу', 'Товариш майор',
-              'Агент ФСБ', 'Black Hat']
-
-weapons = ['', 'Колючий дрин', 'РПГ-7', 'Посох Діда Мороза', 'Бита', '', '', '', '', '', '',
-           'Травмат', 'Діамантове кайло', 'Колода з кіоску', 'Сокира Перуна',
-           'АК-47', 'Гумова палиця', 'Прапор новоросії', 'Експлойт']
-
-defenses = ['', 'Колючий щит', 'Бронежилет вагнерівця', '', '', '', '', '', '', 'Уламок бронетехніки',
-            'Мухомор королівський', '', '', '', '', 'АК-47', 'Поліцейський щит', 'Прапор новоросії', '']
-
-supports = ['', 'Аптечка', '', 'Совєтскоє шампанскоє']
-
-photos = ['https://i.ibb.co/rGd7L5n/rusnya.jpg',
-
-          'https://i.ibb.co/qmkDFTP/1.jpg', 'https://i.ibb.co/hDsXjZb/2.jpg', 'https://i.ibb.co/NNmfy50/3.jpg',
-          'https://i.ibb.co/gZ96zHK/4.jpg', 'https://i.ibb.co/8b0BWyR/5.jpg',
-
-          'https://i.ibb.co/wQhWFcW/1.jpg', 'https://i.ibb.co/5GHPw0Q/2.jpg', 'https://i.ibb.co/2MmzDx1/3.jpg',
-          'https://i.ibb.co/rFwxSsw/4.jpg', 'https://i.ibb.co/6RcHkdw/5.jpg',
-
-          'https://i.ibb.co/h2VBPTx/1.jpg', 'https://i.ibb.co/gDzX80W/2.jpg', 'https://i.ibb.co/xJLTvKV/3.jpg',
-          'https://i.ibb.co/s2ZydPD/4.jpg', 'https://i.ibb.co/wrv51G0/5.png',
-
-          'https://i.ibb.co/NKG6rNT/1.png', 'https://i.ibb.co/jr64ddS/2.png', 'https://i.ibb.co/qWdYP6f/3.png',
-          'https://i.ibb.co/NYPtWsX/4.png', 'https://i.ibb.co/H7dS23v/5.png',
-
-          'https://i.ibb.co/9vYrMRY/1.png', 'https://i.ibb.co/wM3B3Z8/2.png', 'https://i.ibb.co/gM8BpYL/3.png',
-          'https://i.ibb.co/cwbpQZc/4.png', 'https://i.ibb.co/w0kdDkJ/5.png',
-
-          'https://i.ibb.co/FJmyWGW/1.jpg', 'https://i.ibb.co/ZHNrWQ1/2.jpg', 'https://i.ibb.co/YPTDzmZ/3.jpg',
-          'https://i.ibb.co/zQpXqvr/4.jpg', 'https://i.ibb.co/LPW1RzY/5.jpg',
-
-          'https://i.ibb.co/xGD85gj/1.png', 'https://i.ibb.co/px1wrjH/2.png', 'https://i.ibb.co/DYpB1dW/3.png',
-          'https://i.ibb.co/RDJHW38/4.png', 'https://i.ibb.co/kqxKG3f/5.png',
-
-          'https://i.ibb.co/db2zQ5r/1.png', 'https://i.ibb.co/HD8QXLg/2.png', 'https://i.ibb.co/7N3Mycb/3.png',
-          'https://i.ibb.co/D7jMyq1/4.png', 'https://i.ibb.co/FV8xVPk/5.png',
-
-          'https://i.ibb.co/GFMKxrp/9.png', 'https://i.ibb.co/nRpTjWt/10.png',
-          'https://i.ibb.co/8D5kNzV/1.png', 'https://i.ibb.co/31jXXxQ/2.png', 'https://i.ibb.co/zmYDNrn/3.png',
-          'https://i.ibb.co/QDHLRPp/4.png', 'https://i.ibb.co/GntBLKG/5.png', 'https://i.ibb.co/b1jN3GC/6.png',
-          'https://i.ibb.co/QmfJRPP/7.png', 'https://i.ibb.co/9H5Wxfj/8.png']
-
-sudoers = [456514639, 634799543, 379620096]
 
 
 def prepare_to_fight(uid, fn, q):
