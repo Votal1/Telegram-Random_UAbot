ebf0f11d559841f3cab3ac9661523df7e3fa76fc
Votal1 <vitaliypankiv1@gmail.com>
2022-01-11 13:44:06 +0200

    mute command

--- main.py
+++ main.py
@@ -88,6 +88,7 @@ def ban(message):
                 msg = message.reply_to_message.from_user.first_name
                 if message.text.startswith('/unmute'):
                     bot.restrict_chat_member(message.chat.id, message.reply_to_message.from_user.id,
+                                             until_date=datetime.now(),
                                              can_send_messages=True, can_send_media_messages=True, can_send_polls=True,
                                              can_send_other_messages=True, can_add_web_page_previews=True,
                                              can_change_info=True, can_invite_users=True, can_pin_messages=True)
