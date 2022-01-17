1ffd5e8a40752fcda570eebc5e4cb7c9d6862dae
Votal1 <vitaliypankiv1@gmail.com>
2022-01-17 15:51:16 +0200

    weapon and defense fix

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def ban(message):
         if int(r.hget('f' + str(message.chat.id), 'admin')) == 1:
             if bot.get_chat_member(message.chat.id, message.from_user.id).status == 'creator' or \
                     bot.get_chat_member(message.chat.id, message.from_user.id).can_restrict_members is True:
-                msg = message.reply_to_message.from_user.first_name + 'вигнаний з чату'
+                msg = message.reply_to_message.from_user.first_name + ' вигнаний з чату'
                 if message.text.startswith('/unban'):
                     bot.unban_chat_member(message.chat.id, message.reply_to_message.from_user.id, only_if_banned=True)
                     bot.send_message(message.chat.id, message.reply_to_message.from_user.first_name +
