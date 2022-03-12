45d5918f0fafcb5cfb8194156c2794bea88f37b1
Votal1 <vitaliypankiv1@gmail.com>
2022-03-12 14:55:19 +0200

    move to Aiogram

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from redis import Redis
 from os import environ
 from aiogram import Bot, Dispatcher
 
-r = redis.Redis(host=environ.get('REDIS_HOST'), port=int(environ.get('REDIS_PORT')),
-                password=environ.get('REDIS_PASSWORD'), db=0)
+r = Redis(host=environ.get('REDIS_HOST'), port=int(environ.get('REDIS_PORT')),
+          password=environ.get('REDIS_PASSWORD'), db=0)
 
 TOKEN = environ.get('TOKEN')
 bot = Bot(token=TOKEN)
