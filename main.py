3745948312f48b5a4e0b2fef26d88de30c70a211
Votal1 <vitaliypankiv1@gmail.com>
2022-06-18 23:07:16 +0300

    quit fix

--- main.py
+++ main.py
@@ -1162,9 +1162,11 @@ async def crash(message):
 async def quit_from_battle(message):
     try:
         if int(datetime.now().timestamp()) - int(r.hget(message.from_user.id, 'w_ts')) > 1800:
-            cid = r.hget(message.from_user.id, 'in_war').decode()
             r.hdel(message.from_user.id, 'in_war')
+            '''
+            cid = r.hget(message.from_user.id, 'in_war').decode()
             r.srem('fighters_2' + cid, message.from_user.id)
+            '''
             await message.reply('\u2705')
         else:
             await bot.send_message(message.from_user.id, 'Покидати міжчатові битви можна тільки раз в пів години.')
