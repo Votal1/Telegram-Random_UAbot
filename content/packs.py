f38d388bf741c31e51ee5daebff348d6bc53b66e
Votal1 <vitaliypankiv1@gmail.com>
2022-07-04 10:09:35 +0300

    pack update

--- content/packs.py
+++ content/packs.py
@@ -6,7 +6,7 @@ from parameters import vodka
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-def open_pack(uid, cdata):
+def open_pack(uid, cdata, edit):
     markup = InlineKeyboardMarkup()
     msg = ''
     if uid == int(cdata.split('_')[2]):
@@ -31,102 +31,56 @@ def open_pack(uid, cdata):
                 elif ran == [2]:
                     msg = '\u26AA В цьому пакунку лежить якраз те, що потрібно твоєму русаку (класове спорядження)! ' \
                           + icons[cl]
-                    if cl == 1 or cl == 11 or cl == 21:
-                        if int(r.hget(uid, 'weapon')) in (11, 22):
-                            r.hincrby(uid, 's_weapon', 5)
-                            if int(r.hget(uid, 's_weapon')) >= 50:
-                                r.hset(uid, 'weapon', 22)
-                        elif int(r.hget(uid, 'weapon')) != 2:
-                            r.hset(uid, 'weapon', 11)
-                            r.hset(uid, 's_weapon', 5)
-                    elif cl == 2 or cl == 12 or cl == 22:
-                        if int(r.hget(uid, 'weapon')) in (12, 23):
-                            r.hincrby(uid, 's_weapon', 25)
-                            if int(r.hget(uid, 's_weapon')) >= 250:
-                                r.hset(uid, 'weapon', 23)
-                        elif int(r.hget(uid, 'weapon')) != 2:
-                            r.hset(uid, 'weapon', 12)
-                            r.hset(uid, 's_weapon', 25)
-                    elif cl == 3 or cl == 13 or cl == 23:
-                        if int(r.hget(uid, 'weapon')) in (13, 24):
-                            r.hincrby(uid, 's_weapon', 3)
-                            if int(r.hget(uid, 's_weapon')) >= 30:
-                                r.hset(uid, 'weapon', 24)
-                        elif int(r.hget(uid, 'weapon')) != 2:
-                            r.hset(uid, 'weapon', 13)
-                            r.hset(uid, 's_weapon', 3)
-                    elif cl == 4 or cl == 14 or cl == 24:
-                        if int(r.hget(uid, 'weapon')) in (14, 25):
-                            r.hincrby(uid, 's_weapon', 1)
-                            if int(r.hget(uid, 's_weapon')) >= 10:
-                                r.hset(uid, 'weapon', 25)
-                        elif int(r.hget(uid, 'weapon')) != 2:
-                            r.hset(uid, 'weapon', 14)
-                            r.hset(uid, 's_weapon', 1)
-                    elif cl == 5 or cl == 15 or cl == 25:
-                        if int(r.hget(uid, 'weapon')) in (15, 26):
-                            r.hincrby(uid, 's_weapon', 30)
-                            if int(r.hget(uid, 's_weapon')) >= 300:
-                                r.hset(uid, 'weapon', 26)
-                        elif int(r.hget(uid, 'weapon')) != 2:
-                            r.hset(uid, 'weapon', 15)
-                            r.hset(uid, 's_weapon', 30)
-                    elif cl == 6 or cl == 16 or cl == 26:
-                        if int(r.hget(uid, 'defense')) in (16, 17):
-                            r.hincrby(uid, 's_defense', 10)
-                            if int(r.hget(uid, 's_defense')) >= 100:
-                                r.hset(uid, 'defense', 17)
-                        elif int(r.hget(uid, 'defense')) != 2:
-                            r.hset(uid, 'defense', 16)
-                            r.hset(uid, 's_defense', 10)
-                    elif cl == 7 or cl == 17 or cl == 27:
-                        if int(r.hget(uid, 'weapon')) in (17, 28):
-                            r.hincrby(uid, 's_weapon', 8)
-                            if int(r.hget(uid, 's_weapon')) >= 80:
-                                r.hset(uid, 'weapon', 28)
-                        elif int(r.hget(uid, 'weapon')) != 2:
-                            r.hset(uid, 'weapon', 17)
-                            r.hset(uid, 's_weapon', 8)
-                    elif cl == 8 or cl == 18 or cl == 28:
-                        if int(r.hget(uid, 'weapon')) in (18, 29):
-                            r.hincrby(uid, 's_weapon', 2)
-                            if int(r.hget(uid, 's_weapon')) >= 20:
-                                r.hset(uid, 'weapon', 29)
-                        elif int(r.hget(uid, 'weapon')) != 2:
-                            r.hset(uid, 'weapon', 18)
-                            r.hset(uid, 's_weapon', 2)
-                    elif cl == 9 or cl == 19 or cl == 29:
-                        if int(r.hget(uid, 'weapon')) in (19, 30):
-                            r.hincrby(uid, 's_weapon', 8)
-                            if int(r.hget(uid, 's_weapon')) >= 80:
-                                r.hset(uid, 'weapon', 30)
-                        elif int(r.hget(uid, 'weapon')) != 2:
-                            r.hset(uid, 'weapon', 19)
-                            r.hset(uid, 's_weapon', 8)
-                    elif cl == 10 or cl == 20 or cl == 30:
-                        if int(r.hget(uid, 'weapon')) in (20, 31):
-                            r.hincrby(uid, 's_weapon', 10)
-                            if int(r.hget(uid, 's_weapon')) >= 100:
-                                r.hset(uid, 'weapon', 31)
-                        elif int(r.hget(uid, 'weapon')) != 2:
-                            r.hset(uid, 'weapon', 20)
-                            r.hset(uid, 's_weapon', 10)
-                    elif cl == 31 or cl == 32 or cl == 33:
-                        if int(r.hget(uid, 'support')) in (2, 9):
-                            r.hincrby(uid, 's_support', 5)
-                            if int(r.hget(uid, 's_support')) >= 50:
-                                r.hset(uid, 'support', 9)
-                        elif int(r.hget(uid, 'support')) not in (6, 7):
-                            r.hset(uid, 'support', 2)
-                            r.hset(uid, 's_support', 5)
-                    elif cl == 34 or cl == 35 or cl == 36:
-                        if int(r.hget(uid, 'weapon')) in (21, 32):
-                            r.hincrby(uid, 's_weapon', 15)
-                            if int(r.hget(uid, 's_weapon')) >= 150:
-                                r.hset(uid, 'weapon', 32)
-                        elif int(r.hget(uid, 'weapon')) != 2:
-                            r.hset(uid, 'weapon', 21)
-                            r.hset(uid, 's_weapon', 15)
+                    if cl in (1, 11, 21) and int(r.hget(uid, 'weapon')) in (11, 22):
+                        r.hincrby(uid, 's_weapon', 5)
+                        if int(r.hget(uid, 's_weapon')) >= 50:
+                            r.hset(uid, 'weapon', 22)
+                    elif cl in (2, 12, 22) and int(r.hget(uid, 'weapon')) in (12, 23):
+                        r.hincrby(uid, 's_weapon', 25)
+                        if int(r.hget(uid, 's_weapon')) >= 250:
+                            r.hset(uid, 'weapon', 23)
+                    elif cl in (3, 13, 23) and int(r.hget(uid, 'weapon')) in (13, 24):
+                        r.hincrby(uid, 's_weapon', 3)
+                        if int(r.hget(uid, 's_weapon')) >= 30:
+                            r.hset(uid, 'weapon', 24)
+                    elif cl in (4, 14, 24) and int(r.hget(uid, 'weapon')) in (14, 25):
+                        r.hincrby(uid, 's_weapon', 1)
+                        if int(r.hget(uid, 's_weapon')) >= 10:
+                            r.hset(uid, 'weapon', 25)
+                    elif cl in (5, 15, 25) and int(r.hget(uid, 'weapon')) in (15, 26):
+                        r.hincrby(uid, 's_weapon', 30)
+                        if int(r.hget(uid, 's_weapon')) >= 300:
+                            r.hset(uid, 'weapon', 26)
+                    elif cl in (6, 16, 26) and int(r.hget(uid, 'defense')) in (16, 27):
+                        r.hincrby(uid, 's_defense', 10)
+                        if int(r.hget(uid, 's_defense')) >= 100:
+                            r.hset(uid, 'defense', 17)
+                    elif cl in (7, 17, 27) and int(r.hget(uid, 'weapon')) in (17, 28):
+                        r.hincrby(uid, 's_weapon', 8)
+                        if int(r.hget(uid, 's_weapon')) >= 80:
+                            r.hset(uid, 'weapon', 28)
+                    elif cl in (8, 18, 28) and int(r.hget(uid, 'weapon')) in (18, 29):
+                        r.hincrby(uid, 's_weapon', 2)
+                        if int(r.hget(uid, 's_weapon')) >= 20:
+                            r.hset(uid, 'weapon', 29)
+                    elif cl in (9, 19, 29) and int(r.hget(uid, 'weapon')) in (19, 30):
+                        r.hincrby(uid, 's_weapon', 8)
+                        if int(r.hget(uid, 's_weapon')) >= 80:
+                            r.hset(uid, 'weapon', 30)
+                    elif cl in (10, 20, 30) and int(r.hget(uid, 'weapon')) in (20, 31):
+                        r.hincrby(uid, 's_weapon', 10)
+                        if int(r.hget(uid, 's_weapon')) >= 100:
+                            r.hset(uid, 'weapon', 31)
+                    elif cl in (31, 32, 33) and int(r.hget(uid, 'support')) in (2, 9):
+                        r.hincrby(uid, 's_support', 5)
+                        if int(r.hget(uid, 's_support')) >= 50:
+                            r.hset(uid, 'support', 9)
+                    elif cl in (34, 35, 36) and int(r.hget(uid, 'weapon')) in (21, 32):
+                        r.hincrby(uid, 's_weapon', 15)
+                        if int(r.hget(uid, 's_weapon')) >= 150:
+                            r.hset(uid, 'weapon', 32)
+                    elif cl > 0:
+                        markup.add(InlineKeyboardButton(text='Взяти спорядження', callback_data=f'pack_class_{uid}'))
                     else:
                         msg = '\u26AA В цьому пакунку лежать дивні речі, якими русак не вміє користуватись...'
                 elif ran == [3]:
@@ -213,6 +167,137 @@ def open_pack(uid, cdata):
                 msg = 'Недостатньо коштів на рахунку.'
 
             return msg, markup
+
+        elif cdata.startswith('pack_class_'):
+            if cl == 1 or cl == 11 or cl == 21:
+                if int(r.hget(uid, 'weapon')) in (11, 22):
+                    r.hincrby(uid, 's_weapon', 5)
+                    if int(r.hget(uid, 's_weapon')) >= 50:
+                        r.hset(uid, 'weapon', 22)
+                else:
+                    r.hset(uid, 'weapon', 11)
+                    r.hset(uid, 's_weapon', 5)
+            elif cl == 2 or cl == 12 or cl == 22:
+                if int(r.hget(uid, 'weapon')) in (12, 23):
+                    r.hincrby(uid, 's_weapon', 25)
+                    if int(r.hget(uid, 's_weapon')) >= 250:
+                        r.hset(uid, 'weapon', 23)
+                else:
+                    r.hset(uid, 'weapon', 12)
+                    r.hset(uid, 's_weapon', 25)
+            elif cl == 3 or cl == 13 or cl == 23:
+                if int(r.hget(uid, 'weapon')) in (13, 24):
+                    r.hincrby(uid, 's_weapon', 3)
+                    if int(r.hget(uid, 's_weapon')) >= 30:
+                        r.hset(uid, 'weapon', 24)
+                else:
+                    r.hset(uid, 'weapon', 13)
+                    r.hset(uid, 's_weapon', 3)
+            elif cl == 4 or cl == 14 or cl == 24:
+                if int(r.hget(uid, 'weapon')) in (14, 25):
+                    r.hincrby(uid, 's_weapon', 1)
+                    if int(r.hget(uid, 's_weapon')) >= 10:
+                        r.hset(uid, 'weapon', 25)
+                else:
+                    r.hset(uid, 'weapon', 14)
+                    r.hset(uid, 's_weapon', 1)
+            elif cl == 5 or cl == 15 or cl == 25:
+                if int(r.hget(uid, 'weapon')) in (15, 26):
+                    r.hincrby(uid, 's_weapon', 30)
+                    if int(r.hget(uid, 's_weapon')) >= 300:
+                        r.hset(uid, 'weapon', 26)
+                else:
+                    r.hset(uid, 'weapon', 15)
+                    r.hset(uid, 's_weapon', 30)
+            elif cl == 6 or cl == 16 or cl == 26:
+                if int(r.hget(uid, 'defense')) in (16, 17):
+                    r.hincrby(uid, 's_defense', 10)
+                    if int(r.hget(uid, 's_defense')) >= 100:
+                        r.hset(uid, 'defense', 17)
+                else:
+                    r.hset(uid, 'defense', 16)
+                    r.hset(uid, 's_defense', 10)
+            elif cl == 7 or cl == 17 or cl == 27:
+                if int(r.hget(uid, 'weapon')) in (17, 28):
+                    r.hincrby(uid, 's_weapon', 8)
+                    if int(r.hget(uid, 's_weapon')) >= 80:
+                        r.hset(uid, 'weapon', 28)
+                else:
+                    r.hset(uid, 'weapon', 17)
+                    r.hset(uid, 's_weapon', 8)
+            elif cl == 8 or cl == 18 or cl == 28:
+                if int(r.hget(uid, 'weapon')) in (18, 29):
+                    r.hincrby(uid, 's_weapon', 2)
+                    if int(r.hget(uid, 's_weapon')) >= 20:
+                        r.hset(uid, 'weapon', 29)
+                else:
+                    r.hset(uid, 'weapon', 18)
+                    r.hset(uid, 's_weapon', 2)
+            elif cl == 9 or cl == 19 or cl == 29:
+                if int(r.hget(uid, 'weapon')) in (19, 30):
+                    r.hincrby(uid, 's_weapon', 8)
+                    if int(r.hget(uid, 's_weapon')) >= 80:
+                        r.hset(uid, 'weapon', 30)
+                else:
+                    r.hset(uid, 'weapon', 19)
+                    r.hset(uid, 's_weapon', 8)
+            elif cl == 10 or cl == 20 or cl == 30:
+                if int(r.hget(uid, 'weapon')) in (20, 31):
+                    r.hincrby(uid, 's_weapon', 10)
+                    if int(r.hget(uid, 's_weapon')) >= 100:
+                        r.hset(uid, 'weapon', 31)
+                else:
+                    r.hset(uid, 'weapon', 20)
+                    r.hset(uid, 's_weapon', 10)
+            elif cl == 31 or cl == 32 or cl == 33:
+                if int(r.hget(uid, 'support')) in (2, 9):
+                    r.hincrby(uid, 's_support', 5)
+                    if int(r.hget(uid, 's_support')) >= 50:
+                        r.hset(uid, 'support', 9)
+                else:
+                    r.hset(uid, 'support', 2)
+                    r.hset(uid, 's_support', 5)
+            elif cl == 34 or cl == 35 or cl == 36:
+                if int(r.hget(uid, 'weapon')) in (21, 32):
+                    r.hincrby(uid, 's_weapon', 15)
+                    if int(r.hget(uid, 's_weapon')) >= 150:
+                        r.hset(uid, 'weapon', 32)
+                else:
+                    r.hset(uid, 'weapon', 21)
+                    r.hset(uid, 's_weapon', 15)
+
+        elif cdata.startswith('pack_mushroom_'):
+            if int(r.hget(uid, 'intellect')) < 20:
+                if int(r.hget(uid, 'support')) == 6:
+                    r.hincrby(uid, 's_support', 1)
+                else:
+                    r.hset(uid, 'support', 6)
+                    r.hset(uid, 's_support', 1)
+            return edit, None
+
+        elif cdata.startswith('pack_foil_'):
+            if int(r.hget(uid, 'head')) == 1:
+                r.hincrby(uid, 's_head', 20)
+            else:
+                r.hset(uid, 'head', 1)
+                r.hset(uid, 's_head', 20)
+            return edit, None
+
+        elif cdata.startswith('pack_armor_'):
+            if int(r.hget(uid, 'defense')) == 2:
+                r.hincrby(uid, 's_defense', 50)
+            else:
+                r.hset(uid, 'defense', 2)
+                r.hset(uid, 's_defense', 50)
+            return edit, None
+
+        elif cdata.startswith('pack_rpg_'):
+            if int(r.hget(uid, 'weapon')) == 2:
+                r.hincrby(uid, 's_weapon', 1)
+            else:
+                r.hset(uid, 'weapon', 2)
+                r.hset(uid, 's_weapon', 1)
+            return edit, None
 
         else:
             return False
