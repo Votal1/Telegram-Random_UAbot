a0cb2b0bc1fb2cb255ac999b626ff8e408313ba9
Votal1 <vitaliypankiv1@gmail.com>
2022-08-24 01:14:55 +0300

    sale

--- fight.py
+++ fight.py
@@ -1117,6 +1117,12 @@ async def war(cid, location, big_battle):
             if checkClan(win):
                 class_reward += ' \U0001F4FB +3'
                 r.hincrby('c' + r.hget(win, 'clan').decode(), 'technics', 3)
+    elif location == 'Битва на херсонському баштані':
+        class_reward = '\U0001F349: всі учасники битви отримали кавуна.'
+        for member in r.smembers('fighters' + str(cid)):
+            if int(r.hget(member, 'head')) == 0:
+                r.hset(member, 'head', 3)
+                r.hset(member, 's_head', 1)
 
     await sleep(10)
     r.hdel('battle' + str(cid), 'start')
