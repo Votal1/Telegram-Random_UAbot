dbd0ccf876696bcd107fba7332146de45ab5ec80
Votal1 <vitaliypankiv1@gmail.com>
2022-10-13 23:41:33 +0300

    sale

--- main.py
+++ main.py
@@ -990,10 +990,10 @@ async def merchant(message):
 async def donate(message):
     try:
         markup = InlineKeyboardMarkup()
-        url = f'https://randomuabot.diaka.ua/donate?name={message.from_user.id}&amount=30'
+        url = f'https://randomuabot.diaka.ua/donate?name={message.from_user.id}&amount=20'
         markup.add(InlineKeyboardButton(text='\U0001F349 Задонатити', url=url))
         msg = 'Якщо хтось хоче підтримати автора, то можне задонатити і отримати\n\U0001F31F погон російського ' \
-              'генерала, який можна витратити в \n/donate_shop.\n\n\U0001F4B3 Ціна одного погону - 30грн.\n' \
+              'генерала, який можна витратити в \n/donate_shop.\n\n\U0001F4B3 Святкова ціна одного погону - 20грн.\n' \
               '\u274C Не міняйте ім`я (твій айді в тг) в формі оплати, якщо купляєте собі.'
         await bot.send_message(message.from_user.id, msg, reply_markup=markup, protect_content=True)
         if message.chat.type != 'private':
