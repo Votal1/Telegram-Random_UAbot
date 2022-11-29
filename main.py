ba902a232f73bc8e47b950977ec0a6244ab6dcf2
Votal1 <vitaliypankiv1@gmail.com>
2022-11-29 21:40:45 +0200

    nnn reward

--- main.py
+++ main.py
@@ -822,11 +822,36 @@ async def no_nut(message):
     try:
         uid = message.from_user.id
         day = datetime.now().day
-        if day == 1 and str(uid).encode() not in r.smembers('nnn_registered_2022'):
-            name = names[int(r.hget(uid, 'name'))]
-            r.sadd('nnn_registered_2022', uid)
-            r.hset('nnn_2022', uid, 1)
-            await message.reply(f'\U0001F330 {name} приєднюється до челенджу No Nut November!\n1/30')
+        if 1 <= day <= 7:
+            if int(r.hget('nnn_2022', uid)) == 30 and str(uid).encode() in r.smembers('nnn_registered_2022'):
+                r.sadd('nnn_winners_2022', uid)
+                r.srem('nnn_registered_2022', uid)
+                power, mind = 100, 0
+                if str(uid).encode() in r.smembers('nnn_winners_2021'):
+                    power += 100
+                r.hincrby(uid, 'strength', power)
+                if int(r.hget(uid, 'intellect')) < 5:
+                    mind = 3
+                elif int(r.hget(uid, 'intellect')) < 12:
+                    mind = 2
+                elif int(r.hget(uid, 'intellect')) < 20:
+                    mind = 1
+                intellect(mind, uid)
+                if mind > 0:
+                    mind = f' \U0001F9E0 +{mind}'
+                spirit(10000, uid, 0)
+                if r.hexists(uid, 'woman') and int(r.hget(uid, 'woman')) == 1:
+                    r.hincrby(uid, 'childs', 5)
+                    wom = '\U0001F476 +5'
+                else:
+                    r.hset(uid, 'woman', 1)
+                    wom = '\U0001F469\U0001F3FB +1'
+                name = names[int(r.hget(uid, 'name'))]
+                await message.reply(f'{name} успішно завершив челендж No Nut November!\n\U0001F4AA +{power}'
+                                    f'{mind} \U0001F54A +10000 {wom}')
+            else:
+                r.srem('nnn_registered_2022', uid)
+                await message.reply(f'\U0001F534 Твій русак не впорався з цим важким челенджом...')
         elif str(uid).encode() in r.smembers('nnn_registered_2022'):
             nnn_day = int(r.hget('nnn_2022', uid))
             if nnn_day == day:
