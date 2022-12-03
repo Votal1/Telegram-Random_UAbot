68b1eee248b5acd3f9f888f88328852ba16fa704
Votal1 <vitaliypankiv1@gmail.com>
2022-12-03 19:14:11 +0200

    ctop option

--- methods.py
+++ methods.py
@@ -668,8 +668,8 @@ async def ctop(sett, uid, text, cid):
                         place1 = str(place) + '. '
                         place += 1
                         if n == cid:
-                            result = f'\U0001F3C6 Рейтинг цього чату: \n{place1}{title}\n' \
-                                     f'\U0001F3C5 {tier_emoji[tier]}'
+                            result = f'\U0001F3C6 Рейтинг цього чату по перемогах: \n{place1}{title}\n' \
+                                     f'\U0001F3C5 {int(r.hget(222, n))} {tier_emoji[tier]}'
                             break
                     return result
                 else:
