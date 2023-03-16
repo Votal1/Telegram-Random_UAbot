f0aefa0a0a209d3e4071ac10e068a37fa87fbde7
Votal1 <vitaliypankiv1@gmail.com>
2023-03-16 23:48:24 +0200

    sentry update

--- main.py
+++ main.py
@@ -55,8 +55,7 @@ async def gruz200(message):
               f'\n\U0001F681 Гелікоптери: {d[6].text.split(maxsplit=2)[2]}' \
               f'\n\U0001F6A2 Кораблі та катери: {d[9].text.split(maxsplit=3)[3]}'
         await message.reply(title + msg)
-    except Exception as e:
-        print(e)
+    except:
         await message.reply('minusrus.com', disable_web_page_preview=True)
 
 
