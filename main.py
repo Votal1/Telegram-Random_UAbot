d00944ca9c0e14ac9ab798195d7002039e4dac50
Votal1 <vitaliypankiv1@gmail.com>
2023-04-12 21:35:36 +0300

    text fix

--- main.py
+++ main.py
@@ -1061,7 +1061,7 @@ async def donate(message):
         markup = InlineKeyboardMarkup()
         url = f'https://randomuabot.diaka.ua/donate?name={message.from_user.id}&amount=30'
         markup.add(InlineKeyboardButton(text='\U0001F349 Задонатити', url=url))
-        msg = 'Якщо хтось хоче підтримати автора, то можне задонатити і отримати\n\U0001F31F погон російського ' \
+        msg = 'Якщо хтось хоче підтримати автора, то може задонатити і отримати\n\U0001F31F погон російського ' \
               'генерала, який можна витратити в \n/donate_shop.\n\n\U0001F4B3 Ціна одного погону - 30 грн.\n' \
               '\u274C Не міняйте ім`я (твій айді в тг) в формі оплати, якщо купляєте собі.'
         await bot.send_message(message.from_user.id, msg, reply_markup=markup, protect_content=True)
