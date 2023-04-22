9bcb2740bfe1d66f60d4c9f3ae484536e5db006c
Votal1 <vitaliypankiv1@gmail.com>
2023-04-22 10:26:26 +0300

    upgradable items update

--- content/inventory.py
+++ content/inventory.py
@@ -421,7 +421,7 @@ def upgrade_item(cdata, uid):
                     item1 = weapons[item]
                     item2 = weapons[upgrade_to]
                     if item == 16:
-                        r.hset(uid, f's-{item_type}', 1)
+                        r.hset(uid, f's_{item_type}', 1)
                 elif cdata.startswith('tape_defense'):
                     upgrade_to = defenses1[item]
                     item1 = defenses[item]
