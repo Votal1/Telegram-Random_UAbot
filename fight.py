b09d495dac25107d5116e1f10d6196ad4b00e1b9
Votal1 <vitaliypankiv1@gmail.com>
2023-04-29 17:50:56 +0300

    sets update

--- fight.py
+++ fight.py
@@ -1750,7 +1750,7 @@ async def start_raid(cid):
                 hp(-100, member)
 
             if did > 0:
-                chance = 15 * did
+                chance = 20 * did
                 again = choices([0, 1], weights=[100 - chance, chance])[0]
                 if again:
                     reward += '\nРусаки готові до реваншу!'
@@ -1887,7 +1887,7 @@ async def start_raid(cid):
                     hp(100, mem)
                     spirit(10000, mem, 0)
                 if did > 0:
-                    chance = 15 * did
+                    chance = 20 * did
                     again = choices([0, 1], weights=[100 - chance, chance])[0]
                     if again:
                         reward += '\nРусаки готові йти в наступний рейд!'
@@ -2005,7 +2005,7 @@ async def start_raid(cid):
             for member in r.smembers('fighters_3' + str(cid)):
                 hp(-100, member)
             if did > 0:
-                chance = 15 * did
+                chance = 20 * did
                 again = choices([0, 1], weights=[100 - chance, chance])[0]
                 if again:
                     reward += '\nРусаки готові до реваншу!'
