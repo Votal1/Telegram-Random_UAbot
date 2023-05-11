1f1413af96bb97bf400135201bd8428485f7f331
Votal1 <vitaliypankiv1@gmail.com>
2023-05-11 20:47:18 +0300

    new maloros weapon

--- fight.py
+++ fight.py
@@ -217,7 +217,7 @@ async def fight(uid1, uid2, un1, un2, t, mid):
         if sc1 > 0:
             i1, bd1 = schizophrenia(uid1, i1, bd1, True)
             inj1 += '\U0001F464 '
-            if weapon2 == 38:
+            if weapon2 == 38 and c2 in (7, 17, 27) and sc1 > 1:
                 i1, bd1 = schizophrenia(uid1, i1, bd1, True)
         if sc2 > 0:
             i2, bd2 = schizophrenia(uid2, i2, bd2, True)
