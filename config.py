055fd9876e54def854c2254debe7ef1f10157438
Votal1 <vitaliypankiv1@gmail.com>
2023-06-01 14:21:00 +0300

    add username

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 from aiogram import Bot, Dispatcher
 
 r = Redis(host=environ.get('REDIS_HOST'), port=int(environ.get('REDIS_PORT')),
-          password=environ.get('REDIS_PASSWORD'), db=0)
+          username=environ.get('REDIS_USERNAME', 'default'), password=environ.get('REDIS_PASSWORD'), db=0)
 
 TOKEN = environ.get('TOKEN')
 bot = Bot(token=TOKEN)
