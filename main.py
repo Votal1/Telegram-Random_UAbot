1033beef94e1b8f32695f062de01c1c01923243e
Votal1 <vitaliypankiv1@gmail.com>
2025-09-16 20:01:46 +0300

    new alert map

--- main.py
+++ main.py
@@ -6871,7 +6871,7 @@ async def echo(message):
                 if True or not alert_info[1] or alert_info[1].decode() != current_alert or not path.isfile(webp_file):
                     r.hset('alerts_in_ua', 'alert', current_alert)
                     print(current_alert)
-                    generate_map(input_svg, webp_file, current_alert)
+                    generate_map(input_svg, webp_file, "ANNNNNNNNNNNANNNPNNANPNNNNP")
 
             await bot.send_sticker(message.chat.id,
                                    open(webp_file, "rb"),
